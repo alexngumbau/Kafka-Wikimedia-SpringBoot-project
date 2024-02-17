d3434e43478c336292e8b1106c06800c14aeb99d
Alex Mutinda Ngumbau <ALEX.NGUMBAU@equitybank.co.ke>
2024-02-17 09:51:52 +0300

    Initial Commit

---
+++ Kafka-Producer/src/main/java/com/example/springboot/KafkaTopicConfig.java
@@ -0,0 +1,16 @@
+package com.example.springboot;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name("wikimedia_recent_changes")
+                .build();
+    }
+}
---
+++ src/test/java/com/example/KafkaWikimediawaah/KafkaWikimediaWaahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KafkaWikimediawaah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaWikimediaWaahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
